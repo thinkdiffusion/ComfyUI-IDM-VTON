20c88cf2a45646277a6406e47a7460e5f6727cb9
Ubuntu <ubuntu@ip-172-31-31-212.ec2.internal>
2024-05-16 11:14:07 +0000

    requirements command again added

--- __init__.py
+++ __init__.py
@@ -2,3 +2,5 @@ import subprocess
 from .src.nodes_mappings import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
+subprocess.run(f"pip install -r requirements.txt", shell=True)
+
